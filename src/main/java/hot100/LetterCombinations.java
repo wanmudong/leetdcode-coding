98eca9f98f3865017917a75fa5a6af9fb7a01322
chenjiehao <wanmudong@qq.com>
2020-12-13 07:22:51 +0800

    补充neir

--- src/main/java/hot100/LetterCombinations.java
+++ src/main/java/hot100/LetterCombinations.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ */
 public class LetterCombinations {
     public List<String> solution(String digits) {
         List<String> result = new ArrayList<String>();
